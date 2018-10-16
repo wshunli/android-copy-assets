6fdf856bbc76954930885636e33e0bafd3f051ca
wshunli <wshunli@qq.com>
2018-10-16 08:38:11 +0800

    bugfix：AAR has nothing

--- assets/src/main/java/com/wshunli/assets/CopyAssets.java
+++ assets/src/main/java/com/wshunli/assets/CopyAssets.java
@@ -17,16 +17,6 @@ package com.wshunli.assets;
 
 import android.content.Context;
 
-import java.io.BufferedOutputStream;
-import java.io.Closeable;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-
 
 public final class CopyAssets {
 
